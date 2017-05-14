4b69450b4d018cb5fb4f3135f75dc51230d4c553
Vedran Vukelic <vedran.vukelic@gmail.com>
2017-05-14 17:12:22 -0400

    Minor changes.

--- import_from_file.py
+++ import_from_file.py
@@ -1,4 +1,5 @@
 
+import sys
 from os import listdir
 from os.path import isfile, join
 import argparse
@@ -7,7 +8,6 @@ from database import Database
 
 
 def import_stock_data(db, symbol, input_file):
-
     rows = []
 
     for line in input_file:
@@ -15,6 +15,7 @@ def import_stock_data(db, symbol, input_file):
         rows.append(line.rsplit(' ', 5))
 
     db.write_rows('daily_stock_data', symbol, rows)
+    print(symbol)
 
 
 def main():
@@ -26,11 +27,14 @@ def main():
 
     symbols = [f for f in listdir(options.input_directory_path) if isfile(join(options.input_directory_path, f))]
 
-    db = Database('test')
+    db = Database('stock_data')
 
     for symbol in symbols:
         with open(join(options.input_directory_path, symbol)) as input_file:
-            import_stock_data(db, symbol, input_file)
+            try:
+                import_stock_data(db, symbol, input_file)
+            except Exception as e:
+                print('{0} - {1}'.format(symbol, e), file=sys.stderr)
 
 
 if __name__ == '__main__':
