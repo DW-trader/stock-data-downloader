086730f49e81903b8cd952878bfdaf452fbd4333
Vedran <vedran.vukelic@gmail.com>
2017-05-14 15:26:41 +0200

    Comapnies list from different source.

--- tools/companies_from_csv.py
+++ tools/companies_from_csv.py
@@ -9,6 +9,6 @@ with open(input_file_path) as input_file:
     next(input_file)
 
     for line in input_file:
-        info = line.rstrip('\n').split(',')
+        info = line.rstrip('\n').split('|')
 
-        print info[0][1:-1]
+        print info[0]
